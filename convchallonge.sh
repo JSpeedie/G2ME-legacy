d3b2cc5db6acf104e02ff68f83357acbdda58cbf
JSpeedie <julianspeedie@gmail.com>
2017-10-30 00:19:12 -0400

    convchallonge.sh now orders the output according to challonge bracket order

--- convchallonge.sh
+++ convchallonge.sh
@@ -3,7 +3,9 @@ date=$(wget $1 -q -O - \
 	| sort | sed "s/-/ /g" | awk '{print $3, $2, $1}')
 
 wget $1 -q -O - | grep "StoreState" \
-	| grep -oP "(\"display_name\":\"(.*?)\",)|(\"games\":\[\[(.*?)\]\],)" \
-	| sed "s/\"display_name\"://g" | sed "s/\"games\"://g" | sed "s/,$//g" \
+	| grep -oP "(\"identifier\":(.*?),)|(\"display_name\":\"(.*?)\",)|(\"games\":\[\[(.*?)\]\],)" \
+	| sed "s/\"identifier\"://g" | sed "s/\"display_name\"://g" \
+	| sed "s/\"games\"://g" | sed "s/,$//g" \
 	| tr -d '\n' | sed "s/\]\]/\]\]\n/g" \
-	| sed -e "s/\"\"/\" \"/g" -e "s/\[\[/ /g" -e "s/,/ /g" -e "s/\]\]/ $date/g"
+	| sed -e "s/\"\"/\" \"/g" -e "s/\[\[/ /g" -e "s/,/ /g" -e "s/\]\]/ $date/g" \
+	| sort -g | sed "s/^[0-9]\+//g"
