451bcafecfedc0a83a3e9bc4f17d5e70c671b963
JSpeedie <julianspeedie@gmail.com>
2017-10-29 23:48:18 -0400

    convchallonge now attempts to add the date to the output too. Note that this may not always be accurate due to the info available from the challonge page sometimes being inaccurate

--- convchallonge.sh
+++ convchallonge.sh
@@ -1,5 +1,9 @@
+date=$(wget http://challonge.com/potatoes123 -q -O - \
+	| grep -o "[0-9]\{4\}-[0-9]\{1,2\}-[0-9]\{1,2\}" | head -n 1 \
+	| sort | sed "s/-/ /g" | awk '{print $3, $2, $1}')
+
 wget $1 -q -O - | grep "StoreState" \
 	| grep -oP "(\"display_name\":\"(.*?)\",)|(\"games\":\[\[(.*?)\]\],)" \
 	| sed "s/\"display_name\"://g" | sed "s/\"games\"://g" | sed "s/,$//g" \
 	| tr -d '\n' | sed "s/\]\]/\]\]\n/g" \
-	| sed -e "s/\"\"/\" \"/g" -e "s/\[\[/ /g" -e "s/,/ /g" -e "s/\]\]//g"
+	| sed -e "s/\"\"/\" \"/g" -e "s/\[\[/ /g" -e "s/,/ /g" -e "s/\]\]/ $date/g"
