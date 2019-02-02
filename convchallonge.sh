b8d7b170130006df62440a40d7981566829273d5
JSpeedie <julianspeedie@gmail.com>
2019-02-02 14:15:38 -0500

    Now uses jq rather than raw grepping JSON

--- convchallonge.sh
+++ convchallonge.sh
@@ -2,10 +2,6 @@ date=$(wget $1 -q -O - \
 	| grep -o "[0-9]\{4\}-[0-9]\{1,2\}-[0-9]\{1,2\}" | head -n 1 \
 	| sort | sed "s/-/ /g" | awk '{print $3, $2, $1}')
 
-wget $1 -q -O - | grep "StoreState" \
-	| grep -oP "(\"identifier\":(.*?),)|(\"display_name\":\"(.*?)\",)|(\"games\":\[\[(.*?)\]\],)" \
-	| sed "s/\"identifier\"://g" | sed "s/\"display_name\"://g" \
-	| sed "s/\"games\"://g" | sed "s/,$//g" \
-	| tr -d '\n' | sed "s/\]\]/\]\]\n/g" \
-	| sed -e "s/\"\"/\" \"/g" -e "s/\[\[/ /g" -e "s/,/ /g" -e "s/\]\]/ $date/g" \
-	| sort -g | sed "s/^[0-9]\+//g"
+wget $1 -q -O - | grep -oP "\[\'TournamentStore\'\] =.*?\};" \
+	| sed "s/\['TournamentStore'\] = //g" | sed "s/\};$/\}/g" \
+	| jq -j --arg date "$date" '.matches_by_round[] | .[] |  .player1.display_name, " ", .player2.display_name, " ", .scores[0], " ", .scores[1], " ", $date, "\n"'
