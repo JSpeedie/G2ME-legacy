be3e6489e23d81c7296f6dc2d02298041e08a290
JSpeedie <julianspeedie@gmail.com>
2019-03-15 23:41:41 -0400

    Updated compile script to include new files, recompiled

--- compileForWindows.sh
+++ compileForWindows.sh
@@ -1,2 +1,2 @@
-i686-w64-mingw32-gcc G2ME.c glicko2.c entry_file.c -o G2ME32.exe
-x86_64-w64-mingw32-gcc G2ME.c glicko2.c entry_file.c -o G2ME64.exe
+i686-w64-mingw32-gcc G2ME.c glicko2.c entry_file.c fileops.c player_dir.c printing.c sorting.c -o G2ME32.exe
+x86_64-w64-mingw32-gcc G2ME.c glicko2.c entry_file.c fileops.c player_dir.c printing.c sorting.c -o G2ME64.exe
