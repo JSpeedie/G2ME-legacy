ecff6006a1ff8823a6ed39d032ad7fba3690aaa3
JSpeedie <julianspeedie@gmail.com>
2019-10-15 11:46:16 -0400

    created file that uses perl instead of grep -P

---
+++ convchallongeperl.sh
@@ -0,0 +1,7 @@
+date=$(wget $1 -q -O - \
+	| grep -o "[0-9]\{4\}-[0-9]\{1,2\}-[0-9]\{1,2\}" | head -n 1 \
+	| sort | sed "s/-/ /g" | awk '{print $3, $2, $1}')
+
+wget $1 -q -O - | perl -nle 'print $& while m{..TournamentStore...=.*?\};}g' \
+	| sed "s/\['TournamentStore'\] = //g" | sed "s/\};$/\}/g" \
+	| jq -jr --arg date "$date" '.matches_by_round[] | .[] |  "\"", .player1.display_name, "\" \"", .player2.display_name, "\" ", .scores[0], " ", .scores[1], " ", $date, "\n"'
