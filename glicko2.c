1999b639c98e153815f1e8bdd86a327221d0ed39
JSpeedie <julianspeedie@gmail.com>
2024-08-01 11:14:42 -0400

    Cleanup doc strings in glicko library

--- glicko2.c
+++ glicko2.c
@@ -36,114 +36,180 @@ typedef struct player {
 	double vol;
 }Player;
 
-/* Function prototypes because this is not python */
-double _delta(struct player*, double*, int, double*, double*, double);
-double _E(struct player*, double, double);
-double _g(double);
-double _newVol(struct player*, double*, int, double*, double*, double);
-double _v(struct player*, double*, int, double*);
 
-/** Returns the value of a given struct player's rating (known in the
- * glicko2 formulas as μ (mu))
+/* Function prototypes */
+/* double _delta(struct player*, double*, int, double*, double*, double); */
+/* double _E(struct player*, double, double); */
+/* double _g(double); */
+/* double _newVol(struct player*, double*, int, double*, double*, double); */
+/* double _v(struct player*, double*, int, double*); */
+
+
+/** Returns the value of a given struct player's rating (μ) on the Glicko-2
+ * scale. This function paired with the 'getRd()' function constitute Step 2 of
+ * the Glicko-2 Algorithm.
  *
- * \param '*P' the struct player whose rating will be returned
- * \return double representing '*P's rating
+ * \param '*P' the struct player whose rating will be returned.
+ * \return double representing '*P's rating.
  */
 double getRating(struct player* P) {
 	return (P->__rating * 173.7178) + 1500;
 }
 
-/** Sets the given struct player's rating to the given value
+
+/** Sets the given struct player's rating (*r*) on the original scale based
+ * on the given Glicko-2 scale value. This function paired with the 'setRD()'
+ * function constitute Step 8 of the Glicko-2 Algorithm.
  *
- * \param '*P' the struct player whose rating will be changed
- * \param 'rating' the value to set '*P's rating to
- * \return modifies the struct player '*P', returns void
+ * \param '*P' the struct player whose rating will be changed.
+ * \param 'new_rating' the value to set '*P's rating to.
+ * \return modifies the struct player '*P', returns void.
  */
-void setRating(struct player* P, double rating) {
-	P->__rating = (rating - 1500) / 173.7178;
+void setRating(struct player* P, double new_rating) {
+	P->__rating = (new_rating - 1500) / 173.7178;
 }
 
-/** Returns the value of a given struct player's rating deviation (known
- * in the glicko2 formulas as Φ (phi))
+
+/** Returns the value of a given struct player's rating deviation (Φ) on the
+ * Glicko-2 scale. This function paired with the 'setRating()' function
+ * constitute Step 2 of the Glicko-2 Algorithm.
  *
- * \param '*P' the struct player whose RD will be returned
- * \return double representing '*P's rating deviation
+ * \param '*P' the struct player whose RD will be returned.
+ * \return double representing '*P's rating deviation.
  */
 double getRd(struct player* P) {
 	return P->__rd * 173.7178;
 }
 
-/** Sets the given struct player's rating deviation to the given value
+
+/** Sets the given struct player's rating deviation (RD) on the original scale
+ * based on the given Glicko-2 scale value 'new_rd'. This function paired with
+ * the 'setRating()' function constitute Step 8 of the Glicko-2 Algorithm.
  *
- * \param '*P' the struct player whose RD will be changed
- * \param 'rd' the value to set '*P's rating deviation to
- * \return modifies the struct player '*P', returns void
+ * \param '*P' the struct player whose RD will be changed.
+ * \param 'new_rd' the value to set '*P's rating deviation to.
+ * \return modifies the struct player '*P', returns void.
  */
-void setRd(struct player* P, double rd) {
-	P->__rd = rd / 173.7178;
+void setRd(struct player* P, double new_rd) {
+	P->__rd = new_rd / 173.7178;
 }
 
-/** Calculates and updates a struct player's rating deviation for the
- * beginning of a rating period.
+
+/** The Glicko2 *g* function which is used in Steps 3, 4, 7, and by the *E*
+ * function.
  *
- * \param '*P' a struct player representing the player whose RD is to be changed
- * \return modifies struct player '*P', returns void
+ * \param 'phi' a double representing a player's rating deviation (Φ) on the
+ *     Glicko-2 scale.
+ * \return The exact purpose or meaning of the value returned by this function
+ *     is not made clear by the Glicko-2 PDF.
  */
-void _preRatingRD(struct player* P) {
-	P->__rd = sqrt(pow(P->__rd, 2) + pow(P->vol, 2));
+double _g(double phi) {
+	return 1.00 / sqrt(1 + 3 * pow(phi, 2) / pow(M_PI, 2));
 }
 
-/** Updates a sturct player's rating assuming that player undergoes
- * 'rating_list_size' games with the given outcomes and given opponent ratings
- * and RDs.
+
+/** The Glicko2 *E* function which is used in Steps 3, 4, and 7.
  *
- * \param '*P' a struct player representing the player whose rating is to be changed
- * \param '*rating_list' an array of doubles containing (in order) all the ratings of
- *     the opponents '*P' played
- * \param 'rating_list_size' the size of the '*rating_list' array
- * \param '*RD_list' an array od doubles containing (in order) the RDs of the opponents
- *     '*P' played
- * \param '*outcome_list' an array of doubles containing (in order) the outcomes
- *     of the games '*P' played. 1 for '*P' winning, 0.5 for a draw and 0
- *     for a '*P' loss
- * \return modifies the given struct player '*P', returns void
+ * \param '*P' the struct player who will have their estimated variance
+ *     calculated.
+ * \param 'mu_j' a double representing the Glicko-2 scale rating (μ_j) of
+ *     '*P's jth opponent.
+ * \param 'phi_j' a double representing the Glicko-2 scale rating deviation
+ *     (Φ_j) of '*P's jth opponent.
+ * \return The exact purpose or meaning of the value returned by this function
+ *     is not made clear by the Glicko-2 PDF.
  */
-void update_player(struct player *P, \
-	double *rating_list, int rating_list_size, \
-	double *RD_list, double *outcome_list) {
-
-	double v = _v(P, rating_list, rating_list_size, RD_list);
-	P->vol = _newVol(P, rating_list, rating_list_size, RD_list, outcome_list, v);
-	_preRatingRD(P);
-
-	P->__rd = 1 / sqrt((1 / pow(P->__rd, 2)) + (1 / v));
-
-	double tempSum = 0;
-	for (int i = 0; i < rating_list_size; i++) {
-		tempSum += _g(RD_list[i]) * (outcome_list[i] - _E(P, rating_list[i], RD_list[i]));
-	}
-	P->__rating += pow(P->__rd, 2) * tempSum;
+double _E(struct player* P, double mu_j, double phi_j) {
+	return 1 / (1 + exp(-1 * _g(phi_j) * (P->__rating - mu_j)));
 }
 
-/** Calculates the new volatility of a player
+
+/** Calculates the "estimated variance of a the team's/player's rating
+ * based only on game outcomes". This function constitutes Step 3 of the
+ * Glicko-2 algorithm.
  *
- * \param '*P' the struct player who will have their volatility recalculated
+ * \param '*P' the struct player who will have their estimated variance
+ *     calculated
  * \param '*rating_list' an array of doubles representing the ratings of the
  *     opponents '*P' played
  * \param 'rating_list_size' int representing the size of the '*rating_list'
  *     array
- * \param '*RD_list' an array of doubles representing the rating deviations
+ * \param '*rd_list' an array of doubles representing the rating deviations
  *     of the opponents '*P' played
+ * \return a double representing the estimated variance of '*P' based on
+ *     game outcomes
+ */
+double _v(struct player* P, double *rating_list, int rating_list_size, \
+	double *rd_list) {
+
+	double tempSum = 0;
+
+	for (int j = 0; j < rating_list_size; j++) {
+		double tempE = _E(P, rating_list[j], rd_list[j]);
+		tempSum += pow(_g(rd_list[j]), 2) * tempE * (1 - tempE);
+	}
+
+	return 1 / tempSum;
+}
+
+
+/** Calculates the delta (∆) quantity which representing the estimated
+ * improvement in rating based only on game outcomes. This function constitutes
+ * Step 4 of the Glicko-2 algorithm.
+ *
+ * \param '*P' the struct player the function will calculate the delta of.
+ * \param '*rating_list' an array of doubles representing the ratings of the
+ *     opponents '*P' played.
+ * \param 'rating_list_size' int representing the size of the '*rating_list'
+ *     array.
+ * \param '*rd_list' an array of doubles representing the rating deviations
+ *     of the opponents '*P' played.
  * \param '*outcome_list' an array of doubles containing (in order) the outcomes
  *     of the games '*P' played. 1 for '*P' winning, 0.5 for a draw and 0
- *     for a '*P' loss
- * \param // TODO fill out this param
- * \return a double representing '*P's new volatility
+ *     for a '*P' loss.
+ * \param 'v' a double representing the quantity *v* calculated in Step 3 from
+ *     this series of outcomes representing the estimated variance of the
+ *     player.
+ * \return a double containing the estimated improvement in '*P's rating.
+ */
+double _delta(struct player* P, double *rating_list, int rating_list_size, \
+	double *rd_list, double *outcome_list, double v) {
+
+	double tempSum = 0;
+
+	for (int i = 0; i < rating_list_size; i++) {
+		tempSum += _g(rd_list[i]) \
+			* (outcome_list[i] - _E(P, rating_list[i], rd_list[i]));
+	}
+
+	return v * tempSum;
+}
+
+
+/** Calculates the new volatility (σ') of a player. This function constitutes
+ * Step 5 of the Glicko-2 algorithm.
+ *
+ * \param '*P' the struct player who will have their volatility recalculated.
+ * \param '*rating_list' an array of doubles representing the ratings of the
+ *     opponents '*P' played.
+ * \param 'rating_list_size' int representing the size of the '*rating_list'
+ *     array.
+ * \param '*rd_list' an array of doubles representing the rating deviations
+ *     of the opponents '*P' played.
+ * \param '*outcome_list' an array of doubles containing (in order) the outcomes
+ *     of the games '*P' played. 1 for '*P' winning, 0.5 for a draw and 0
+ *     for a '*P' loss.
+ * \param 'v' the quantity *v* calculated in Step 3 from a series of outcomes
+ *     representing the estimated variance of the player.
+ * \return a double representing '*P's new volatility.
  */
 double _newVol(struct player* P, double *rating_list, int rating_list_size, \
-	double *RD_list, double *outcome_list, double v) {
+	double *rd_list, double *outcome_list, double v) {
 
-	double delta = _delta(P, rating_list, rating_list_size, RD_list, outcome_list, v);
+	double delta = \
+		_delta(P, rating_list, rating_list_size, rd_list, outcome_list, v);
+
+	// Step 5, item 1
 	double a = log(pow(P->vol, 2));
 	double tau = P->_tau;
 	double x0 = a;
@@ -164,88 +230,64 @@ double _newVol(struct player* P, double *rating_list, int rating_list_size, \
 	return exp(x1 / 2);
 }
 
-/** Calculates the delta value which represents the "estimated improvement in
- * rating by comparing the pre-period rating to the performance rating based
- * only on game outcomes"
+
+/** Calculates a provisional rating deviation (Φ*) representing the new
+ * pre-rating-period value and updates the struct player's rating deviation to
+ * it. This function constitutes Step 6 of the Glicko-2 algorithm.
  *
- * \param '*P' the struct player the function will calculate the delta of
- * \param '*rating_list' an array of doubles representing the ratings of the
- *     opponents '*P' played
- * \param 'rating_list_size' int representing the size of the '*rating_list'
- *     array
- * \param '*RD_list' an array of doubles representing the rating deviations
- *     of the opponents '*P' played
- * \param '*outcome_list' an array of doubles containing (in order) the outcomes
- *     of the games '*P' played. 1 for '*P' winning, 0.5 for a draw and 0
- *     for a '*P' loss
- * \param 'v' a double representing the result of a '_v' function call
- * \return a double containing the estimated improvement in '*P's rating
+ * \param '*P' a struct player representing the player whose rating deviation
+ *     (Φ) is to be changed.
+ * \return modifies struct player '*P', returns void.
  */
-double _delta(struct player* P, double *rating_list, int rating_list_size,
-	double *RD_list, double *outcome_list, double v) {
-
-	double tempSum = 0;
-
-	for (int i = 0; i < rating_list_size; i++) {
-		tempSum += _g(RD_list[i]) * (outcome_list[i] - _E(P, rating_list[i], RD_list[i]));
-	}
-
-	return v * tempSum;
+void _preRatingRD(struct player* P) {
+	P->__rd = sqrt(pow(P->__rd, 2) + pow(P->vol, 2));
 }
 
-/** Calculates the "estimated variance of a the team's/player's rating
- * based only on game outcomes"
- *
- * \param '*P' the struct player who will have their estimated variance
- *     calculated
- * \param '*rating_list' an array of doubles representing the ratings of the
- *     opponents '*P' played
- * \param 'rating_list_size' int representing the size of the '*rating_list'
- *     array
- * \param '*RD_list' an array of doubles representing the rating deviations
- *     of the opponents '*P' played
- * \return a double representing the estimated variance of '*P' based on
- *     game outcomes
- */
-double _v(struct player* P, double *rating_list, int rating_list_size, double *RD_list) {
-
-	double tempSum = 0;
-
-	for (int i = 0; i < rating_list_size; i++) {
-		double tempE = _E(P, rating_list[i], RD_list[i]);
-		tempSum += pow(_g(RD_list[i]), 2) * tempE * (1 - tempE);
-	}
-
-	return 1 / tempSum;
-}
-
-/** The Glicko2 E function which is a helper function for '_v'
- *
- * \param '*P' the struct player who will have their estimated variance
- *     calculated
- * \param 'p2rating' a double representing the rating of '*P's opponent
- * \param 'p2RD' a double representing the rating deviation of '*P's opponent
- * \return //TODO write this return
- */
-double _E(struct player* P, double p2rating, double p2RD) {
-	return 1 / (1 + exp(-1 * _g(p2RD) * (P->__rating - p2rating)));
-}
-
-/** The Glicko2 g function which is a helper function for '_E'
- *
- * \param 'RD' a double representing a player's rating deviation
- * \return //TODO write this return
- */
-double _g(double RD) {
-	return 1.00 / sqrt(1 + 3 * pow(RD, 2) / pow(M_PI, 2));
-}
 
 /* Applies Step 6 of the algorithm. Use this for players who did not compete
  * in the rating period.
  *
- * \param '*P' the struct player who did not compete
+ * \param '*P' the struct player who did not compete.
  * \return void
 */
 void did_not_compete(struct player* P) {
 	_preRatingRD(P);
+}
+
+
+/** Updates a struct player's rating assuming that player undergoes
+ * 'rating_list_size' games with the given outcomes (s_1, ... , s_m), opponent
+ * ratings (μ_1, ... , μ_m), and opponent rating deviations (Φ_1, ... , Φ_m).
+ * This function (loosely) constitutes Step 7 of the Glicko-2 algorithm.
+ *
+ * \param '*P' a struct player representing the player whose rating is to be
+ *     changed.
+ * \param '*rating_list' an array of doubles containing (in order) all the
+ *     ratings of the opponents '*P' played.
+ * \param 'rating_list_size' the size of the '*rating_list' array.
+ * \param '*rd_list' an array of doubles containing (in order) the RDs of the
+ *     opponents '*P' played.
+ * \param '*outcome_list' an array of doubles containing (in order) the
+ *     outcomes of the games '*P' played. 1 for '*P' winning, 0.5 for a draw
+ *     and 0 for a '*P' loss.
+ * \return modifies the given struct player '*P', returns void.
+ */
+void update_player(struct player *P, double *rating_list, \
+	int rating_list_size, double *rd_list, double *outcome_list) {
+
+	double v = _v(P, rating_list, rating_list_size, rd_list);
+
+	P->vol = \
+		_newVol(P, rating_list, rating_list_size, rd_list, outcome_list, v);
+
+	_preRatingRD(P);
+
+	P->__rd = 1 / sqrt((1 / pow(P->__rd, 2)) + (1 / v));
+
+	double tempSum = 0;
+	for (int i = 0; i < rating_list_size; i++) {
+		tempSum += _g(rd_list[i]) \
+			* (outcome_list[i] - _E(P, rating_list[i], rd_list[i]));
+	}
+	P->__rating += pow(P->__rd, 2) * tempSum;
 }
--- glicko2.h
+++ glicko2.h
@@ -16,17 +16,17 @@ typedef struct player {
 }Player;
 
 /* Function prototypes because this is not python */
-void did_not_compete(struct player*);
 double getRating(struct player*);
-double getRd(struct player*);
 void setRating(struct player*, double);
+double getRd(struct player*);
 void setRd(struct player*, double);
-void update_player(struct player*, double*, int, double*, double*);
-double _delta(struct player*, double*, int, double*, double*, double);
-double _E(struct player*, double, double);
 double _g(double);
-void _preRatingRD(struct player*);
-double _newVol(struct player*, double*, int, double*, double*, double);
+double _E(struct player*, double, double);
 double _v(struct player*, double*, int, double*);
+double _delta(struct player*, double*, int, double*, double*, double);
+double _newVol(struct player*, double*, int, double*, double*, double);
+void _preRatingRD(struct player*);
+void did_not_compete(struct player*);
+void update_player(struct player*, double*, int, double*, double*);
 
 #endif
