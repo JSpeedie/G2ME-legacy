dc758c97eb4d43c6947612d38a8eacaeb4cc49db
JSpeedie <julianspeedie@gmail.com>
2026-06-14 12:12:23 -0400

    Modified `G2ME-server` and `G2ME-client` build processes to use the three-part, preface, template, top-level makefile structure

--- G2ME-server.mk
+++ G2ME-server.mk
@@ -1,77 +1,21 @@
-PREFIX=/usr/local
-MANPREFIX=/usr/share/man
-# Flags
-# TODO: maybe create a separate variable for the debug flags as well as a
-# separate variable for optimization flags for the release build
-CFLAGS=-Wall -ggdb3
-# CFLAGS=-Wall
-# Compiler and linker
-CC=gcc
 
 #------------------------------------------------------------------------------
-#   Manually Specified Information
+#   Manually specified information
 #------------------------------------------------------------------------------
-# `NAME_GS` cannot refer to multiple binaries
-NAME_GS:=G2ME-server
-SRCS_GS:=server.c clientserverutil.c
-# `INCS_GS` and `LIBS_GS` expect you to specify, respectively, include flags and
-# their arguments and library flags and their arguments
-INCS_GS:=
-LIBS_GS:=
-
-#------------------------------------------------------------------------------
-#   Automatically Specified Information
-#------------------------------------------------------------------------------
-OBJS_GS:=${SRCS_GS:.c=.o}
-OBJS_GS_E=$(wildcard $(OBJS_GS))
-DEPS_GS:=${SRCS_GS:.c=.d}
-DEPS_GS_E=$(wildcard $(DEPS_GS))
-# `MAN_GS` cannot refer to multiple files
-# Populate `MAN_GS` with all the array elements of `NAME_GS` but with a ".1.gz"
-# appended to each element.
-MAN_GS:=$(patsubst %, %.1.gz, $(NAME_GS))
+# `NAME` cannot contain more than one element
+NAME:=G2ME-server
+SRCS:=server.c clientserverutil.c
+# `-I`, `-L`, `-l`
+INCS:=
+LIBRARIES:=
+LIBS:=
 
 #------------------------------------------------------------------------------
-#   Additional Rules
+#   Include template makefile(s)
 #------------------------------------------------------------------------------
-G2ME-server: $(OBJS_GS)
-	$(CC) $(CFLAGS) $^ $(INCS_GS) $(LIBS_GS) -o $@
-
-%.o: %.c
-	$(CC) $(CFLAGS) $< $(INCS_GS) -c -o $@
-
-# A rule to create a dependency file for every `.c` file
-%.d: %.c
-	$(CC) $(CFLAGS) $< $(INCS_GS) -MM -MF $@
-
-# Include all the dependency files IF the commandline goal given to Make was
-# not `clean`
-ifneq ($(MAKECMDGOALS),clean)
-include $(DEPS_GS)
-endif
-
-.PHONY: G2ME-server-install
-G2ME-server-install: $(NAME_GS)
-	mkdir -p $(PREFIX)/bin
-	cp -f $(NAME_GS) $(PREFIX)/bin/$(NAME_GS); \
-	chmod 755 $(PREFIX)/bin/$(NAME_GS); \
-	mkdir -p $(MANPREFIX)/man1/
-	cp -f $(MAN_GS) $(MANPREFIX)/man1/$(MAN_GS); \
-
-.PHONY: G2ME-server-uninstall
-G2ME-server-uninstall:
-	rm -f $(PREFIX)/bin/$(NAME_GS)
-	rm -f $(MANPREFIX)/man1/$(MAN_GS)
-
-.PHONY: G2ME-server-clean
-G2ME-server-clean:
-ifneq ($(strip $(OBJS_GS_E)),)
-	rm -f $(OBJS_GS_E)
-else
-	$(info No $(NAME_GS) .o files to remove)
-endif
-ifneq ($(strip $(DEPS_GS_E)),)
-	rm -f $(DEPS_GS_E)
-else
-	$(info No $(NAME_GS) .d files to remove)
-endif
+# Create Make recipes for `$(NAME)_release`, `$(NAME)_release_install`,
+# `$(NAME)_release_uninstall`, and `$(NAME)_release_clean`
+include template-release.mk
+# Create Make recipes for `$(NAME)_debug`, `$(NAME)_debug_install`,
+# `$(NAME)_debug_uninstall`, and `$(NAME)_debug_clean`
+include template-debug.mk
